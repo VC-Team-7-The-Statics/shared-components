d88f3ca35fd447e2c5a1e0b688af0be333a02b78
jinwookShin <0201jinwook_@gmail.com>
2022-06-04 15:44:01 +0900

    feat: Button 컴포넌트 제작

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.scss";
+import Button from "./components/Button";
 
 function App() {
   return (
     <div className="container">
       <h1>Shared Components</h1>
+      <Button text={"hello"} />
     </div>
   );
 }
