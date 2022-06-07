dfd8c1b51add16654832cb6781db6c22ac70ab06
jinwookShin <0201jinwook_@gmail.com>
2022-06-07 17:18:05 +0900

    style: index.js에 새 컴포넌트 추가

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,19 +1,21 @@
-import Button from "./components/Button";
+import Button01 from "./components/Button";
+import Button02 from "./components/Button";
 import Select from "./components/Select";
 import Option from "./components/Option";
 import Input from "./components/Input";
-import Checkbox from "./components/Checkbox";
 import Checkboxes from "./components/Checkboxes";
 import Textarea from "./components/Textarea";
 import RequestCard from "./components/RequestCard";
+import Title from "./components/Title";
 
 export {
-  Button,
+  Button01,
+  Button02,
   Select,
   Option,
   Input,
-  Checkbox,
   Checkboxes,
   Textarea,
   RequestCard,
+  Title,
 };
